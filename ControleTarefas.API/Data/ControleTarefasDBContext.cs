30986234d639bde23396634e083697ba77d78dfc
Monyk Paola Penafor <91429002+MonkPP@users.noreply.github.com>
2023-05-04 21:11:40 -0300

    update1

---
+++ ControleTarefas.API/Data/ControleTarefasDBContext.cs
@@ -0,0 +1,19 @@
+﻿using ControleTarefas.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleTarefas.API.Data
+{
+    public class ControleTarefasDBContext : DbContext
+    {
+        public ControleTarefasDBContext(DbContextOptions<ControleTarefasDBContext> options) : base(options) { 
+        
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
